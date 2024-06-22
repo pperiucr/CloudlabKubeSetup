9ba4b17c8c6611a96e8f38fe50bcd3df2919956d
pperiucr <prakash.perimbeti@email.ucr.edu>
2024-06-22 12:34:36 -0700

    Update kubeinstall.sh

--- kubeinstall.sh
+++ kubeinstall.sh
@@ -1,11 +1,28 @@
-#Signing key and Software repo
-sudo mkdir -p -m 755 /etc/apt/keyrings && 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg &&
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list &&
+#!/bin/bash
+#Installing KUBERNETES
+# Setting up security key
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 #Install & setup kubernetes
 #Install Cube tools and mark the packages on hold
-sudo apt install kubeadm kubelet kubectl &&
-sudo apt-mark hold kubeadm kubelet kubectl &&
+sudo apt install kubeadm kubelet kubectl
+sudo apt-mark hold kubeadm kubelet kubectl
 kubeadm version
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+#content="overlay \\nbr_netfilter"
+#sudo bash -c "echo '$content' | sed 's/\\\\n/\\n/' > /etc/modules-load.d/containerd.conf"
+echo 'overlay
+br_netfilter' | sudo tee /etc/modules-load.d/containerd.conf > /dev/null
+sudo modprobe overlay
+sudo modprobe br_netfilter
+sudo bash -c 'cat <<EOF > /etc/sysctl.d/kubernetes.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF'
+sudo sysctl --system
+
 
