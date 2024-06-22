4d2db92d8dda9df2fb34648ba39f9c95aaef023b
pperiucr <prakash.perimbeti@email.ucr.edu>
2024-06-22 12:29:48 -0700

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
 #install & setup docker
-sudo apt update && sudo apt install docker.io -y && sudo systemctl enable docker && sudo systemctl start docker
+sudo apt update
+sudo apt install docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
