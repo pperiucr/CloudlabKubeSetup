e0686ede0fe6c8e50f99fe670e71fed3d7f1e7d5
pperiucr <prakash.perimbeti@email.ucr.edu>
2024-07-17 18:21:47 -0700

    Update dockersetup.sh
    Oran

--- dockersetup.sh
+++ dockersetup.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-#install & setup docker
-#sudo apt update
-#sudo apt install docker.io -y
-#sudo systemctl enable docker
-#sudo systemctl start docker
+install & setup docker
+sudo apt update
+sudo apt install docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
 
-sudo apt update
-sudo apt install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-compose-plugin -y
-docker compose version
